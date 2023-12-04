f3c503407d6fb8e91b051d081f2c40ef834475a8
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2023-12-04 14:36:05 +0100

    esercizio caricato

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,41 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args){
-
-        Map<String,String> forme = new HashMap<>();
-        forme.put("Q","quadrato");
-        forme.put("C","cerchio");
-        forme.put("T","triangolo");
-
-        System.out.println("La mappa Forme:" + forme);
-
-        Map<String, String> stagioni = Map.of("IN","inverno","AT","autunno","ES","estate","PM","primavera");
-
-        System.out.println("La mappa Stagioni:" + stagioni);
-
-        Map<String,String> giorni = Map.ofEntries(
-                new AbstractMap.SimpleEntry<>("LU","lunedì"),
-                new AbstractMap.SimpleEntry<>("MA","martedì"),
-                new AbstractMap.SimpleEntry<>("ME","mercoledì"),
-                new AbstractMap.SimpleEntry<>("GIO","giovedì"),
-                new AbstractMap.SimpleEntry<>("VE","venerdì"),
-                new AbstractMap.SimpleEntry<>("SA","sabato"),
-                new AbstractMap.SimpleEntry<>("DO","domenica")
-        );
-
-        System.out.println("La mappa Giorni:" + giorni);
+        forme();
+        stagioni();
+        giorni();
 
     }
+
+        public static void forme(){
+            Map<String,String> forme = new HashMap<>();
+            forme.put("Q","quadrato");
+            forme.put("C","cerchio");
+            forme.put("T","triangolo");
+
+            System.out.println("La mappa Forme:" + forme);
+        }
+
+        public static void stagioni(){
+            Map<String, String> stagioni = Map.of("IN","inverno","AT","autunno","ES","estate","PM","primavera");
+
+            System.out.println("La mappa Stagioni:" + stagioni);
+        }
+
+        public static void giorni(){
+            Map<String,String> giorni = Map.ofEntries(
+                    new AbstractMap.SimpleEntry<>("LU","lunedì"),
+                    new AbstractMap.SimpleEntry<>("MA","martedì"),
+                    new AbstractMap.SimpleEntry<>("ME","mercoledì"),
+                    new AbstractMap.SimpleEntry<>("GIO","giovedì"),
+                    new AbstractMap.SimpleEntry<>("VE","venerdì"),
+                    new AbstractMap.SimpleEntry<>("SA","sabato"),
+                    new AbstractMap.SimpleEntry<>("DO","domenica")
+            );
+
+            System.out.println("La mappa Giorni:" + giorni);
+        }
+
+
+
 }
